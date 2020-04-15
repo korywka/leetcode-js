00207137b39a7be14aac913e2ab28dd83ff236cc
korovka <neporozhniy@gmail.com>
2020-04-15 20:03:13 +0300

    wip

--- searching/binary.js
+++ searching/binary.js
@@ -18,18 +18,35 @@ function binarySearch(sortedArray, seekElement) {
 }
 
 function binarySearchFirst(sortedArray, startIndex, endIndex, x) {
-  console.log(startIndex, endIndex, x);
-  let midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+  let middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
 
-  if ((midIndex === 0 || sortedArray[midIndex - 1] < x) && sortedArray[midIndex] === x) {
-    return midIndex;
+  if (sortedArray[middleIndex] === x && (middleIndex === 0 || sortedArray[middleIndex - 1] < x)) {
+    return middleIndex;
   }
 
   if (startIndex <= endIndex) {
-    if (x > sortedArray[midIndex]) {
-      return binarySearchFirst(sortedArray, midIndex + 1, endIndex, x);
+    if (x > sortedArray[middleIndex]) {
+      return binarySearchFirst(sortedArray, middleIndex + 1, endIndex, x);
     } else {
-      return binarySearchFirst(sortedArray, startIndex, midIndex - 1, x);
+      return binarySearchFirst(sortedArray, startIndex, middleIndex - 1, x);
+    }
+  }
+
+  return -1;
+}
+
+function binarySearchLast(sortedArray, startIndex, endIndex, x) {
+  let middleIndex = startIndex + Math.floor(((endIndex - startIndex) / 2));
+
+  if (sortedArray[middleIndex] === x && (middleIndex === sortedArray.length - 1 || sortedArray[middleIndex + 1] > x)) {
+    return middleIndex;
+  }
+
+  if (startIndex <= endIndex) {
+    if (x >= sortedArray[middleIndex]) {
+      return binarySearchLast(sortedArray, middleIndex + 1, endIndex, x);
+    } else {
+      return binarySearchLast(sortedArray, startIndex, middleIndex - 1, x);
     }
   }
 
